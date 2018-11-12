1ce3578f519080f558fc922b289524429ab09822
Tylyps <j.chmielewski@o2.pl>
2018-11-12 22:08:36 +0100

    Instal react and react-dom modules

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import  isSenior, { isAdult, canDring } from './person.js';
-// import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running!!!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
+const template = React.createElement('p', {}, 'TEST 123');
 
-console.log(isAdult(21));
-console.log(isAdult(15));
-console.log(canDring(21));
-console.log(canDring(6));
-console.log(isSenior(40));
-console.log(isSenior(70));
-
+ReactDOM.render(template, document.getElementById('app'));
